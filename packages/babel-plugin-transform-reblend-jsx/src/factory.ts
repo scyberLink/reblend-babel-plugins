8380a5dc982ba2091c11f21727300ae8330a4b8e
Emmanuel Paul Elom <elomemmanuel007@gmail.com>
2025-06-01 18:20:09 +0100

    feat: restructure babel-plugin-transform-reblend-jsx
    - Add constants for Reblend identifiers in a new constants.ts file.
    - Refactor create-plugin.ts to utilize new utility functions and constants.
    - Introduce factory functions for creating element and fragment construct calls.
    - Implement findReblendImportName to dynamically retrieve the import name for Reblend.
    - Create utility functions for handling JSX attributes and converting identifiers.

---
+++ packages/babel-plugin-transform-reblend-jsx/src/factory.ts
@@ -0,0 +1,91 @@
+import { accumulateAttribute, getTag, get } from "./utils";
+import * as t from "@babel/types";
+import type { PluginPass, NodePath } from "@babel/core";
+import type {
+  JSXFragment,
+  JSXElement,
+  JSXAttribute,
+  JSXSpreadAttribute,
+  ObjectExpression,
+  CallExpression,
+} from "@babel/types";
+import { REBLEND_CONSTRUCT_ID, REBLEND_IMPORT_NAME_ID } from "./constants";
+
+// Builds JSX Fragment <></> into
+// Reblend.construct(Reblend, null, ...children)
+export function createFragmentConstructCall(
+  path: NodePath<JSXFragment>,
+  file: PluginPass,
+) {
+  return call(file, REBLEND_CONSTRUCT_ID, [
+    t.identifier(get(file, REBLEND_IMPORT_NAME_ID)),
+    t.nullLiteral(),
+    // @ts-expect-error JSXSpreadChild has been transformed in convertAttributeValue
+    ...t.react.buildChildren(path.node),
+  ]);
+}
+
+/**
+ * The logic for this is quite terse. It's because we need to
+ * support spread elements. We loop over all attributes,
+ * breaking on spreads, we then push a new object containing
+ * all prior attributes to an array for later processing.
+ */
+export function createPropsObjectFromAttributes(
+  file: PluginPass,
+  path: NodePath<JSXElement>,
+  attribs: NodePath<JSXAttribute | JSXSpreadAttribute>[],
+) {
+  const props: ObjectExpression['properties'] = [];
+  const found = Object.create(null);
+
+  for (const attr of attribs) {
+    const { node } = attr;
+    const name =
+      t.isJSXAttribute(node) && t.isJSXIdentifier(node.name) && node.name.name;
+    accumulateAttribute(props, attr);
+  }
+
+  return props.length === 1 &&
+    t.isSpreadElement(props[0]) &&
+    // If an object expression is spread element's argument
+    // it is very likely to contain __proto__ and we should stop
+    // optimizing spread element
+    !t.isObjectExpression(props[0].argument)
+    ? props[0].argument
+    : props.length > 0
+      ? t.objectExpression(props)
+      : t.nullLiteral();
+}
+
+// Builder
+// Builds JSX into:
+// Production: Reblend.construct(type, arguments, children)
+// Development: Reblend.construct(type, arguments, children, source, self)
+export function createElementConstructCall(
+  path: NodePath<JSXElement>,
+  file: PluginPass,
+) {
+  const openingPath = path.get('openingElement');
+
+  return call(file, REBLEND_CONSTRUCT_ID, [
+    getTag(openingPath),
+    createPropsObjectFromAttributes(
+      file,
+      path,
+      openingPath.get('attributes'),
+    ),
+    // @ts-expect-error JSXSpreadChild has been transformed in convertAttributeValue
+    ...t.react.buildChildren(path.node),
+  ]);
+}
+
+function call(
+  pass: PluginPass,
+  name: string,
+  args: CallExpression['arguments'],
+) {
+  const construct = get(pass, name);
+  const node = t.callExpression(construct(), args);
+  return node;
+}
---
+++ packages/babel-plugin-transform-reblend-jsx/src/utils.ts
@@ -0,0 +1,176 @@
+import type { PluginPass, NodePath } from '@babel/core';
+import * as t from '@babel/types';
+import type {
+  ObjectExpression,
+  JSXAttribute,
+  JSXSpreadAttribute,
+  JSXOpeningElement,
+  Class,
+  Identifier,
+  CallExpression,
+} from '@babel/types';
+
+// Plugin state helpers
+export const get = (pass: PluginPass, name: string) =>
+  pass.get(`babel-plugin-reblend-jsx/${name}`);
+export const set = (pass: PluginPass, name: string, v: any) =>
+  pass.set(`babel-plugin-reblend-jsx/${name}`, v);
+
+// Utility: Check for __proto__ property in object expressions
+function hasProto(node: t.ObjectExpression) {
+  return node.properties.some(
+    value =>
+      t.isObjectProperty(value, { computed: false, shorthand: false }) &&
+      (t.isIdentifier(value.key, { name: '__proto__' }) ||
+        t.isStringLiteral(value.key, { value: '__proto__' })),
+  );
+}
+
+// Converts JSX identifier/member/namespace to Babel AST node
+function convertJSXIdentifier(
+  node: t.JSXIdentifier | t.JSXMemberExpression | t.JSXNamespacedName,
+  parent: t.JSXOpeningElement | t.JSXMemberExpression,
+): t.ThisExpression | t.StringLiteral | t.MemberExpression | t.Identifier {
+  if (t.isJSXIdentifier(node)) {
+    if (node.name === 'this' && t.isReferenced(node, parent)) {
+      return t.thisExpression();
+    } else if (t.isValidIdentifier(node.name, false)) {
+      // @ts-expect-error cast AST type to Identifier
+      node.type = 'Identifier';
+      return node as unknown as t.Identifier;
+    } else {
+      return t.stringLiteral(node.name);
+    }
+  } else if (t.isJSXMemberExpression(node)) {
+    return t.memberExpression(
+      convertJSXIdentifier(node.object, node),
+      convertJSXIdentifier(node.property, node),
+    );
+  } else if (t.isJSXNamespacedName(node)) {
+    // If the flag "throwIfNamespace" is false, print XMLNamespace like string literal
+    return t.stringLiteral(`${node.namespace.name}:${node.name.name}`);
+  }
+  // todo: this branch should be unreachable
+  return node;
+}
+
+// Converts JSX attribute value to Babel AST node
+function convertAttributeValue(
+  node: t.JSXAttribute['value'] | t.BooleanLiteral,
+) {
+  if (t.isJSXExpressionContainer(node)) {
+    return node.expression;
+  } else {
+    return node;
+  }
+}
+
+// Accumulates JSX attributes and spreads into an object properties array
+export function accumulateAttribute(
+  array: ObjectExpression['properties'],
+  attribute: NodePath<JSXAttribute | JSXSpreadAttribute>,
+) {
+  if (t.isJSXSpreadAttribute(attribute.node)) {
+    const arg = attribute.node.argument;
+    // Collect properties into props array if spreading object expression
+    if (t.isObjectExpression(arg) && !hasProto(arg)) {
+      array.push(...arg.properties);
+    } else {
+      array.push(t.spreadElement(arg));
+    }
+    return array;
+  }
+
+  const value = convertAttributeValue(
+    attribute.node.name.name !== 'key'
+      ? attribute.node.value || t.booleanLiteral(true)
+      : attribute.node.value,
+  );
+
+  if (attribute.node.name.name === 'key' && value === null) {
+    throw attribute.buildCodeFrameError(
+      'Please provide an explicit key value. Using "key" as a shorthand for "key={true}" is not allowed.',
+    );
+  }
+
+  if (
+    t.isStringLiteral(value) &&
+    !t.isJSXExpressionContainer(attribute.node.value)
+  ) {
+    value.value = value.value.replace(/\n\s+/g, ' ');
+    // "raw" JSXText should not be used from a StringLiteral because it needs to be escaped.
+    delete value.extra?.raw;
+  }
+
+  if (t.isJSXNamespacedName(attribute.node.name)) {
+    // @ts-expect-error mutating AST
+    attribute.node.name = t.stringLiteral(
+      attribute.node.name.namespace.name + ':' + attribute.node.name.name.name,
+    );
+  } else if (t.isValidIdentifier(attribute.node.name.name, false)) {
+    // @ts-expect-error mutating AST
+    attribute.node.name.type = 'Identifier';
+  } else {
+    // @ts-expect-error mutating AST
+    attribute.node.name = t.stringLiteral(attribute.node.name.name);
+  }
+
+  array.push(
+    t.inherits(
+      t.objectProperty(
+        // @ts-expect-error The attribute.node.name is an Identifier now
+        attribute.node.name,
+        value,
+      ),
+      attribute.node,
+    ),
+  );
+  return array;
+}
+
+// Gets the tag for a JSX opening element (string or identifier)
+export function getTag(openingPath: NodePath<JSXOpeningElement>) {
+  const tagExpr = convertJSXIdentifier(openingPath.node.name, openingPath.node);
+
+  let tagName: string;
+  if (t.isIdentifier(tagExpr)) {
+    tagName = tagExpr.name;
+  } else if (t.isStringLiteral(tagExpr)) {
+    tagName = tagExpr.value;
+  }
+
+  if (t.react.isCompatTag(tagName)) {
+    return t.stringLiteral(tagName);
+  } else {
+    return tagExpr;
+  }
+}
+
+// Converts a string like "Reblend.construct" to a member expression or call
+export function toMemberExpression(
+  id: string,
+  isFragment: boolean,
+): Identifier | CallExpression {
+  if (isFragment) {
+    return (
+      id
+        .split('.')
+        .map(name => t.identifier(name))
+        // @ts-expect-error - The Array#reduce does not have a signature
+        // where the type of initial value differs from callback return type
+        .reduce((object, property) => t.memberExpression(object, property))
+    );
+  }
+  return t.callExpression(
+    t.memberExpression(
+      id
+        .split('.')
+        .map(name => t.identifier(name))
+        // @ts-expect-error - The Array#reduce does not have a signature
+        // where the type of initial value differs from callback return type
+        .reduce((object, property) => t.memberExpression(object, property)),
+      t.identifier('bind'),
+    ),
+    [t.thisExpression()],
+  );
+}
